cc9492e277f0bffec81d192726f9bd10c3311415
cuongdq.payment <cuongdq.payment@gmail.com>
2018-12-02 04:03:46 +0700

    HEROKU Deployment by cuongdq

--- src/services/apiService.ts
+++ src/services/apiService.ts
@@ -9,7 +9,7 @@ import jwt from 'jsonwebtoken';
 @Injectable()
 export class ApiService {
   
-  public authenticationServer = 'https://cuongdq-oauth.herokuapp.com/';
+  public authenticationServer = '';//'https://cuongdq-oauth.herokuapp.com';
   public clientKey = new NodeRSA({ b: 512 }, { signingScheme: 'pkcs1-sha256' }); //for decrypte
   public midleKey =  new NodeRSA(null, { signingScheme: 'pkcs1-sha256' }); //for test
   public serverKey = new NodeRSA(null, { signingScheme: 'pkcs1-sha256' }); //for crypte
