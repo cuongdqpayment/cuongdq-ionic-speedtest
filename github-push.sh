8d58054a2a8efeec3fafec1df7eab5c2f8871ee0
cuongdqpayment <cuongdq.payment@gmail.com>
2018-12-04 16:50:41 +0700

    cuongdq push by sh

--- github-push.sh
+++ github-push.sh
@@ -1,5 +1,8 @@
 git init
 git add .
 git commit -m "cuongdq push by sh"
-#git remote add origin https://github.com/cuongdqpayment/heroku-ionic-web.git
-git push -u origin master
+#git remote -v
+#git remote remove github 
+#(thay chu origin = github de quan ly de hon)
+#git remote add github https://github.com/cuongdqpayment/cuongdq-ionic-speedtest.git
+git push -u github master
