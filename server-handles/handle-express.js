0a6abf9256db4d9340eb5c0adfa9efd16250951e
cuongdq.payment <cuongdq.payment@gmail.com>
2018-12-09 05:28:55 +0700

    cuongdq push by sh

--- server-handles/handle-express.js
+++ server-handles/handle-express.js
@@ -2,9 +2,13 @@ class HandlesExpress{
     constructor() {}
     cors(req, res, next) {
         //console.log(req.url) //phan no request la gi PUT, OPTIONS, DELETE,
-        res.header("Access-Control-Allow-Methods", "POST, GET");
-        res.header('Access-Control-Allow-Origin', '*'); //cho phep truy cap
-         //res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE");
+        res.header('Access-Control-Allow-Origin', 'http://localhost:8080'); //cho phep truy cap
+        res.header('Access-Control-Allow-Origin', 'http://localhost:9235'); //cho phep truy cap
+        res.header('Access-Control-Allow-Origin', 'http://localhost:8*'); //cho phep truy cap
+        res.header('Access-Control-Allow-Origin', '*.herokuapp.com'); //cho phep truy cap
+        res.header('Access-Control-Allow-Origin', '*.mobifone.vn'); //cho phep truy cap
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
         //res.header("Access-Control-Allow-Credentials", true);
         next();
       }
